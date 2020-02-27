8e55c669629462c1378b8292f2160b08f335b8a9
Kergo <sskamenov@gmail.com>
2020-02-27 13:43:10 +0100

    Added Cart Dropdown Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebasae.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.style.scss';
 
@@ -30,6 +32,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
